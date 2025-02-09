c6496d105451e7f83fa0f7bae21c498d82a9f210
mikhaelHan <hancharuk270185@mail.ru>
2025-02-09 17:41:17 +0300

    feat: add and set routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import Main from '../components/pages/Main';
 
 const App: React.FC = () => {
-  return <Main />;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import '../styles/index.css';
 import App from './App.tsx';
 import ErrorBoundary from './providers/ErrorBoundary.tsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>,
 );
